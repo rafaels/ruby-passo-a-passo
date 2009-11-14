52374ab2ff3555e7dfed54a04647391e0401b0ec
Rafael Souza <rafael@maisweb.org>
2009-11-14 01:05:34 -0300

    hash

---
+++ hash.rb
@@ -0,0 +1,8 @@
+# Pares de chave-valor
+
+pessoa = {:nome => 'elomar', :idade => 18, 'altura' => 1.73, 'sabe' => ['ruby', '...']}
+
+sala = {:id => 'lab5', 'professor' => pessoa}
+
+puts pessoa[:nome]
+puts sala['professor'][:nome]
