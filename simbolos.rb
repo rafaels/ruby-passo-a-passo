f949267515e6ae6c31290fc2fd1bb99eef6fcedc
Rafael Souza <rafael@maisweb.org>
2009-11-14 00:12:01 -0300

    simbolos

---
+++ simbolos.rb
@@ -0,0 +1,16 @@
+# Simbolos se parecem com strings, mas tem algumas diferenças
+
+:isso_eh_um_simbolo
+
+:dois_pontos
+
+puts :simbolo
+
+puts "string".object_id
+puts "string".object_id
+
+puts :simbolo.object_id
+puts :simbolo.object_id
+
+
+# Qual a conclusão!?
