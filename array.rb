0a3ac9bd2dc24e5ee36d47e07afe47ebbd46dc64
Rafael Souza <rafael@maisweb.org>
2009-11-14 00:57:29 -0300

    array

---
+++ array.rb
@@ -0,0 +1,12 @@
+# array de numeros
+puts [1, 2, 3]
+
+# array de strings
+puts ['oi', "voce", "aí"]
+
+# array de qualquer coisa
+arr = [2, '4', [false, true], :simbolo] 
+
+puts arr[2][0]
+puts arr.first
+puts arr.last
