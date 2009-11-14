75a54f7e4378357f4acd8cec6be6aac22bafea36
Rafael Souza <rafael@maisweb.org>
2009-11-14 00:29:35 -0300

    metodos

---
+++ metodos.rb
@@ -0,0 +1,10 @@
+# coisas que fazem alguma coisa :)
+# por exemplo, puts, .to_f, .reverse,
+# =, -, *, /, e outros
+
+puts 'hello '.+ 'world'
+puts (10.* 9).+ 9
+
+puts "arara".reverse
+# brincadeira... =p
+puts "Hey You".reverse
