2bf803cd5e6b875ea6d069b8dd61f3f1e3b521ca
Rafael Souza <rafael@maisweb.org>
2009-11-13 23:52:59 -0300

    Numeros

---
+++ numeros.rb
@@ -0,0 +1,28 @@
+# Começando por números, mas antes, vamos ver um método que vai ajudar bastante
+# O método "puts"
+# Ele imprime na tela o que vier depois dele.
+# Vamos dar uma olhada no uso dele com números!
+
+# Inteiros
+puts 6
+puts -50
+puts 77777777777777777777777
+
+# Floats
+puts 1.618
+puts -43.86
+puts 0.0
+
+# Um pouco de aritimética
+puts 1 + 3
+puts 3 + 4
+puts 5 + 8
+puts 3 / 2
+
+puts 1.0 + 3.0
+puts 3.0+ 4.0
+puts 5.0 + 8.0
+puts 3.0 / 2.0
+
+
+# Por que a diferença?
